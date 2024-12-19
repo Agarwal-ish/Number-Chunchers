6499a29fb6420f7f54c43ff813b3e48702652717
ashutoshkumar1020 <115533717+ashutoshkumar1020@users.noreply.github.com>
2024-12-19 07:46:02 +0530

    main.py file updated

--- main.py
+++ main.py
@@ -4,9 +4,4 @@ import Preprocessor
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Streamlit App Header
-st.set_page_config(page_title="Crime Analysis Dashboard", page_icon="🔍")
-st.image("crime.webp", width=130, use_column_width=False) 
-st.title("🔍 Crime Analysis Dashboard")
-st.subheader("Analyze and visualize crime data to uncover insights.")
-st.write("---")
+
